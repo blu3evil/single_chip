31e099f7d0ba07885e5ee5e221fa8cc763443189
bluefat <53940932+blu3evil@users.noreply.github.com>
2021-11-23 23:36:19 +0800

    new version of the segment program
    last new version of segment program updated was removed because of the wrong operation,It 's the third time I upload it!

--- DELAY.h
+++ DELAY.h
@@ -13,24 +13,6 @@ void Delay10ms()		//@12.000MHz
 	} while (--i);
 }
 
-/*延时函数2,用于控制闪烁频率:1HZ*/
-void Delay500ms()		//@12.000MHz
-{
-	unsigned char i, j, k;
-
-	_nop_();
-	_nop_();
-	i = 23;
-	j = 205;
-	k = 120;
-	do
-	{
-		do
-		{
-			while (--k);
-		} while (--j);
-	} while (--i);
-}
 
 /*延时函数(用于数码管消抖)*/
 void Delay1ms()		//@12.000MHz
--- segment_display.c
+++ segment_display.c
@@ -1,17 +1,16 @@
 #include <reg52.h>
 #include <DELAY.h>
 
-#define DEFAULT_SEG_EW 20		   	//东西方向数码管默认时间
-#define DEFAULT_SEG_SN 23		   	//南北方向数码管默认时间
 #define DEFAULT_FLICKER_EW 3		//东西方向黄灯默认闪烁时间
 #define DEFAULT_FLICKER_SN 3		//南北方向黄灯默认闪烁时间
 
-unsigned char data seg_EW = 20;    //东西方向数码管秒数
-unsigned char data seg_SN = 2;    //南北方向数码管时间
-unsigned char data flicker_EW = 3; //东西方向黄灯闪烁时间
-unsigned char data flicker_SN = 3; //南北方向黄灯闪烁时间
+unsigned char data default_sec_seg = 20;
+unsigned char data sec_seg = 20; 	//数码管倒计时
+unsigned char data flicker_EW = 3; 	//东西方向黄灯闪烁时间
+unsigned char data flicker_SN = 3; 	//南北方向黄灯闪烁时间
 
-bit mode_chosen = 1;	//目前选择的方向标志,置1为东西，置0为南北(除手动调整程序中会随当前黄灯亮起方向自动改变)
+bit mode_chosen = 1;	//状态标志，置1为状态1，置0为状态2
+bit mode = 1; 			//用于选择数码管显示模式，置1显示数码管倒计时，置0显示黄灯闪烁倒计时
 
 sbit red_EW = P2^6;      //东西方向红灯
 sbit yellow_EW = P2^5;	  //东风方向黄灯
@@ -54,6 +53,66 @@ void Config_Timer0(void)
 	ET0 = 1;		//使能T0中断
 }
 
+/*更新数码管状态函数，用于恢复暂停后数码管倒数状态*/
+void Update(bit mode)
+{
+	if (mode)		//显示状态切换倒计时
+	{
+		P1 = 0xff;		//清零P1控制位	
+		P0 = 0x00;		//清零P0数码管输入
+		P1 = 0xfe;		//选择东西方向数码管十位
+		P0 = seg_table[sec_seg/10];
+		Delay1ms();
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xfd;		//选择东西方向数码管个位
+		P0 = seg_table[sec_seg%10];
+		Delay1ms();
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xfb;  	//选择南北方向数码管十位
+		P0 = seg_table[sec_seg/10];
+		Delay1ms();
+
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xf7;  	//选择南北方向数码管个位
+		P0 = seg_table[sec_seg%10];
+		Delay1ms();
+	}
+	else
+	{
+		P1 = 0xff;				//清零P1控制位	
+		P0 = 0x00;				//清零P0数码管输入
+		P1 = 0xfe;				//选择东西方向数码管十位
+		P0 = seg_table[flicker_EW/10];
+		Delay1ms();
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xfd;				//选择东西方向数码管个位
+		P0 = seg_table[flicker_EW%10];
+		Delay1ms();
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xfb;  	//选择南北方向数码管十位
+		P0 = seg_table[flicker_SN/10];
+		Delay1ms();
+
+
+		P1 = 0xff;
+		P0 = 0x00;
+		P1 = 0xf7;  	//选择南北方向数码管个位
+		P0 = seg_table[flicker_SN%10];
+		Delay1ms();
+	}
+	
+	
+}
 
 /*停止函数，用于增加/减少时间时暂停进程*/
 void Pause(void)
@@ -72,6 +131,7 @@ void Flicker_add(void)
 	{
 		flicker_SN += 1;
 	}
+	mode = 0;
 }
 
 /*闪烁时间减少函数*/
@@ -85,37 +145,38 @@ void Flicker_sub(void)
 	{
 		flicker_SN -= 1;
 	}
+	mode = 0;
 }
 
 /*红绿灯码管倒计时增*/
 void Seg_add(void)
 {
-	if (mode_chosen)
+	if (sec_seg == 99)
 	{
-		seg_EW += 1;
-	}
-	else
-	{
-		seg_SN += 1;
-	}
+		sec_seg = 0;	//形成0~99闭环
+		return;
+	}			
+	sec_seg++;
+	default_sec_seg++;
+	mode = 1;
 }
 
 /*红绿灯数码管倒计时减*/
 void Seg_sub(void)
 {
-	if (mode_chosen)
+	if (sec_seg == 0)
 	{
-		seg_EW -= 1;
+		sec_seg = 99;
+		return;
 	}
-	else
-	{
-		seg_SN -= 1;
-	}
+	sec_seg--;
+	default_sec_seg--;
+	mode = 1;
 }
 
 void Led_Config(void)
 {
-	if (mode_chosen)	//状态1，东西方向先黄灯
+	if (mode_chosen)	//状态1，南北方向黄灯将闪烁
 	{
 		red_EW = 0;
 		green_EW = 1;
@@ -137,89 +198,42 @@ void Led_Config(void)
 	}
 }
 
-/*更新数码管状态函数，用于恢复暂停后数码管倒数状态*/
-void update()
-{
-	P1 = 0xff;		//清零P1控制位	
-	P0 = 0x00;		//清零P0数码管输入
-	P1 = 0xfe;		//选择东西方向数码管十位
-	P0 = seg_table[seg_EW/10];
-	Delay1ms();
-	
-	P1 = 0xff;
-	P0 = 0x00;
-	P1 = 0xfd;		//选择东西方向数码管个位
-	P0 = seg_table[seg_EW%10];
-	Delay1ms();
-	
-	P1 = 0xff;
-	P0 = 0x00;
-	P1 = 0xfb;  	//选择南北方向数码管十位
-	P0 = seg_table[seg_SN/10];
-	Delay1ms();
-	
-		
-	P1 = 0xff;
-	P0 = 0x00;
-	P1 = 0xf7;  	//选择南北方向数码管个位
-	P0 = seg_table[seg_SN%10];
-	Delay1ms();
-	
-}
+
 
 /*定时器T0中断函数*/
 void InterruptTimer0(void) interrupt 1
 {
 	/*黄灯控制部分*/
 	static unsigned char count0 = 0;
-	if (seg_EW == seg_SN || (seg_EW == 0 && seg_SN != 0))		//东西方向黄灯开始闪烁
+	if (mode_chosen && sec_seg <= flicker_SN)		//南北方向黄灯闪烁(即将进入状态2)
+	{
+		red_SN = 0;
 		if (count0 % 10 == 0)				//黄灯一秒闪烁一次,0.5s亮，0.5s灭
-			yellow_EW = !yellow_EW;
-	if (seg_EW == seg_SN || (seg_SN == 0 && seg_EW != 0))		//南北方向黄灯闪烁
-		if (count0 % 10 == 0)
 			yellow_SN = !yellow_SN;
+	}
+	 if (!mode_chosen && sec_seg <= flicker_EW)					//东西方向黄灯闪烁
+	{
+		red_EW = 0;
+		if (count0 % 10 == 0)
+			yellow_EW = !yellow_EW;
+	}
 	
 
 	/*数码管部分*/
 	/*黄灯倒计时结束切换状态1,2*/
-	if (seg_EW == 0 && seg_SN == 0)		//黄灯倒计时归零
+	if (sec_seg == 0)		//黄灯倒计时归零
 	{
-		if (mode_chosen)				//东西方向黄灯倒计时归零
-		{
-			seg_EW = DEFAULT_SEG_SN;		//第一次启动默认东西方向先黄灯，
-			seg_SN = DEFAULT_SEG_EW;		//故将东西方向与南北方向默认时间互换以切换黄灯反向
-			mode_chosen = !mode_chosen;					//黄灯亮起方向变为南北
-		}
-		else
-		{
-			seg_EW = DEFAULT_SEG_EW;		
-			seg_SN = DEFAULT_SEG_SN;
-			mode_chosen = !mode_chosen;
-		}
-		update();
+		mode_chosen = !mode_chosen;					//切换状态
+		sec_seg = default_sec_seg;
+		Update(1);
 		Led_Config();
 	}
+		
 	
-	/*黄灯开始倒计时*/
-	if (seg_EW == 0 && seg_SN !=0)		//东西方向黄灯倒计时等于南北方向红绿灯倒计时
-	{
-		seg_EW = seg_SN;
-		update();
-	}
-	
-	if (seg_SN == 0 && seg_EW !=0)		//南北方向黄灯倒计时等于东西方向红绿灯倒计时
-	{
-		seg_SN = seg_EW;
-		update();
-	}	
-	
-
 	if (count0 == 20)	//一秒时
 	{
 		count0 = 0;						//清空count0
-		seg_EW -= 1;
-		seg_SN -= 1;
-		update();
+		sec_seg--;
 	}
 	count0++;
 }
@@ -281,6 +295,7 @@ void Key_Scan(void)
 			Pause();
 			Config_Timer0();
 			mode_chosen = !mode_chosen;
+			mode = 1;
 		}
 	}
 
@@ -289,11 +304,8 @@ void Key_Scan(void)
 		Delay10ms();
 		if (k6 == 0)
 		{
-			if (seg_EW > seg_SN)
-				mode_chosen = 1;
-			else
-				mode_chosen = 0;
 			TR0 = 1;	//启动定时器0，让时间开始流动
+			mode = 1;
 		}
 	}
 
@@ -310,7 +322,7 @@ void main()
 	while (1)
 	{
 		Key_Scan();
-		update();
+		Update(mode);
 	}
 }
 
